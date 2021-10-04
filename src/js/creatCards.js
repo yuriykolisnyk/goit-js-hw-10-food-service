c9fd9d3cbea820ac30a037fb28c4140b6248ffc7
yuriykolisnyk <ykolisnyk@gmail.com>
2021-10-04 16:15:32 +0300

    cread cards

--- src/js/creatCards.js
+++ src/js/creatCards.js
@@ -1,1 +1,7 @@
+import cardsTemplate from '../templates/menuCard.hbs';
 import menuData from '../json/menu.json';
+
+const menu = cardsTemplate(menuData);
+const menuItem = document.querySelector('.js-menu');
+
+menuItem.insertAdjacentHTML('beforeend', menu);
